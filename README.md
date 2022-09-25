24cebf77eacd82f11a6f591e87b6ef4b34fc9d50
Kassio Kennedy <95866074+KaessSantos@users.noreply.github.com>
2022-09-25 11:33:52 -0300

    coloquei o link

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Pizza
  Compra de pizza, 
+ <a href"https://kaesssantos.github.io/E-commerce-Pizza/"> Projeto
--- treino.js
+++
@@ -1,7 +0,0 @@
-let num = [1, 5, 10, 7];
-
-num.map(function(elemento, índice, array){
-    console.log(elemento)
-    console.log(índice)
-    console.log(array)
-}, 80);
